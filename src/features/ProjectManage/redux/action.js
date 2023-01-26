6016f00cab667929f7c1d048da4ddcee4c50852e
sybao <luongsybao2505@gmail.com>
2023-01-26 13:58:03 +0700

    arange action

--- src/features/ProjectManage/redux/action.js
+++ src/features/ProjectManage/redux/action.js
@@ -2,18 +2,7 @@ import requestor from "app/api";
 import { apiPath } from "app/apiPath";
 import actions from "./type";
 
-export const fetchAllProject = () => async (next) => {
-  try {
-    const res = await requestor({
-      method: "GET",
-      url: apiPath.ALL_PROJECT,
-    });
-    next({ type: actions.SET_ALL_PROJECT, payload: res.data.content });
-  } catch (error) {
-    console.log(error);
-  }
-};
-
+//USER
 export const updateUserProfileAction = (data) => async (next) => {
   try {
     const res = await requestor({
@@ -26,6 +15,19 @@ export const updateUserProfileAction = (data) => async (next) => {
     console.log(error);
     if (error.response) alert(error.response.data.message);
     throw error;
+  }
+};
+
+//PROJECT
+export const fetchAllProject = () => async (next) => {
+  try {
+    const res = await requestor({
+      method: "GET",
+      url: apiPath.ALL_PROJECT,
+    });
+    next({ type: actions.SET_ALL_PROJECT, payload: res.data.content });
+  } catch (error) {
+    console.log(error);
   }
 };
 
@@ -55,6 +57,7 @@ export const updateProjectDetail = (projectId, data) => async () => {
     console.log(error);
   }
 };
+
 export const deleteProject = (projectId) => async () => {
   try {
     const res = await requestor({
@@ -68,6 +71,7 @@ export const deleteProject = (projectId) => async () => {
     throw alert(error.response.data.content);
   }
 };
+
 export const createProject = (data) => async () => {
   try {
     const res = await requestor({
@@ -81,6 +85,8 @@ export const createProject = (data) => async () => {
     throw alert(error.response.data.content);
   }
 };
+
+// TASK
 export const createTask = (data) => async () => {
   try {
     const res = await requestor({
